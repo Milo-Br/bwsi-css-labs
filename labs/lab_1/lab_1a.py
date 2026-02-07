8f05d5959417a27f5e31fd01d9abe5b79a15ae19
mbrano-hash <milo@branovacki.com>
2026-02-07 17:21:50 -0600

    Added Introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Milo B" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f"Hi, I'm Milo, a sophomore from Chicago, Illinois. A fun fact/hobby is that I enjoy playing golf and tennis, and I like collecting watches. I look forward to exploring this course more!")
 
 if __name__ == "__main__":
     main()
